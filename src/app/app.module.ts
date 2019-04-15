ee87e3049f95ae20df9dd3a90cf398ab7ed03ca0
Luis Inacio <lrs.inacio@gmail.com>
2019-04-15 18:49:04 +0100

    Initial structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {PanelModule} from 'primeng/panel';
+import {HttpClientModule} from '@angular/common/http';
+import {CommonModule} from '@angular/common';
 
 import { AppComponent } from './app.component';
+import {ButtonModule} from 'primeng/button';
+import {DropdownModule, InputTextModule} from 'primeng/primeng';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    PanelModule,
+    HttpClientModule,
+    ButtonModule,
+    InputTextModule,
+    ReactiveFormsModule,
+    DropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
